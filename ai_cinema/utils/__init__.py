e962a88b848f97ceb2b96c1a6f1a7fc7ac7b593e
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2024-12-20 14:08:39 +0300

    Update __init__.py

--- ai_cinema/utils/__init__.py
+++ ai_cinema/utils/__init__.py
@@ -1,1 +1,30 @@
+"""
+Utility functions for AI-Cinema.
+"""
 
+from .dialectal import (
+    detect_dialect,
+    convert_dialect,
+    normalize_dialectal_text,
+    DIALECT_MAPPINGS
+)
+
+from .evaluation import (
+    calculate_cpm,
+    calculate_bleu,
+    calculate_dialectal_accuracy,
+    evaluate_cultural_preservation,
+    evaluate_linguistic_fluency
+)
+
+__all__ = [
+    'detect_dialect',
+    'convert_dialect',
+    'normalize_dialectal_text',
+    'DIALECT_MAPPINGS',
+    'calculate_cpm',
+    'calculate_bleu',
+    'calculate_dialectal_accuracy',
+    'evaluate_cultural_preservation',
+    'evaluate_linguistic_fluency'
+]
