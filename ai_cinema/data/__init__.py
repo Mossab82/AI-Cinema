7b4a4a49d9a2b5fa8523a86071e023836745be49
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2024-12-20 12:02:56 +0300

    Update __init__.py

--- ai_cinema/data/__init__.py
+++ ai_cinema/data/__init__.py
@@ -1,1 +1,33 @@
 
+from .loaders import (
+    load_cultural_knowledge,
+    load_dialectal_corpus,
+    load_wordnet,
+    load_verbnet,
+    load_emotion_lexicon,
+    load_name_lexicon,
+    load_scripts
+)
+
+from .processors import (
+    preprocess_text,
+    extract_cultural_elements,
+    detect_dialect,
+    normalize_arabic,
+    tokenize_arabic
+)
+
+__all__ = [
+    'load_cultural_knowledge',
+    'load_dialectal_corpus',
+    'load_wordnet',
+    'load_verbnet',
+    'load_emotion_lexicon',
+    'load_name_lexicon',
+    'load_scripts',
+    'preprocess_text',
+    'extract_cultural_elements',
+    'detect_dialect',
+    'normalize_arabic',
+    'tokenize_arabic'
+]
